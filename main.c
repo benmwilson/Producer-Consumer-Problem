b8b20edb6dcc4b928934f650001ebf1a06745d4b
Buttertoastt <77471167+Buttertoastt@users.noreply.github.com>
2022-02-27 22:12:15 -0800

    timer added, working on pid

--- main.c
+++ main.c
@@ -4,9 +4,10 @@
 #include <semaphore.h>
 #include <time.h>
 #include <unistd.h>
+#include <sys/wait.h>
 
-#define TIMER = 1000
-#define THREAD_NUM  3
+#define SLEEP_TIMEOUT = 10000
+#define THREAD_NUM  10
 #define MAX 5    //Number of slots initially available in the buffer
 int buffer[MAX];        
 int fill = 0;
@@ -15,46 +16,59 @@ int take = 0;
 sem_t empty;
 sem_t full;
 pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
+clock_t start,end,duration;
 
 void *producer(void *args) {
-    int c1;
-    for (int i = 0; i < MAX; i++)
-    {
-       
-    
-        c1 = 1;
+    for (int i = 1; i < MAX+1; i++){
+        start = clock();
         int product = rand(); // Produce a random product
         sem_wait(&empty);
         // scope of lock reduced
         pthread_mutex_lock(&mutex);
         buffer[fill] = product;
-        printf("Producer: produced request id %d, length x seconnds at %d\n",  buffer[fill], fill);
+        end = clock();
+        duration = ((float)(end-start))/(CLOCKS_PER_SEC/1000);
+        printf("Producer %d: produced request id %d, length %.f seconds at %d\n", getppid(), buffer[fill], (float)duration, fill);
+        start =clock();
         fill = (fill + 1);
         pthread_mutex_unlock(&mutex);
         sem_post(&full);
-        c1++;
-        sleep(1); 
+        sleep(10);
+        end = clock();
+        duration = ((float)(end-start))/(CLOCKS_PER_SEC/1000);
+        printf("Producer: sleeping for %.f milliseconds\n", (float)duration ); 
     }
-    
 }
 
 void *consumer(void *args) {
-    for (int i = 1; i < MAX+1; i++)
-    {    
-        sem_wait(&full);
-        // scope of lock reduced
-        pthread_mutex_lock(&mutex);
-        int product = buffer[take];
-        pthread_mutex_unlock(&mutex);
-        sem_post(&empty);
-        printf("Consumer %d: consumed item %d from %d\n", i, product, take);
-        take =(take + 1)%MAX;
-        sleep(1);   
-    }
+    int pid = fork();
+    if(pid == 0){
+            sleep(1);
+        }
+        for (int i = 1; i < MAX; i++){
+            start = clock();
+            sem_wait(&full);
+            // scope of lock reduced
+            pthread_mutex_lock(&mutex);
+            int product = buffer[take];
+            pthread_mutex_unlock(&mutex);
+            end = clock();
+            duration = ((float)(end-start))/(CLOCKS_PER_SEC/1000);  
+            printf("Consumer %d: assigned request ID %d for the next %.f seconds (caller: %d)item from %d\n", getpid(), product, (float)duration , i,  take);
+            sem_post(&empty);
+            start =clock();
+            take =(take + 1)%MAX;
+            sleep(10);
+            end = clock();
+            duration = ((float)(end-start))/(CLOCKS_PER_SEC/1000);
+            printf("Consumer: sleeping for %.f milliseconds\n", (float)duration ); 
+            wait(NULL);
+            return 0;   
+        }
 }
 
-int main(int argc, char* argv[])
-{
+int main(int argc, char* argv[]){
+
     pthread_t thread[THREAD_NUM];
     pthread_mutex_init(&mutex, NULL);
     sem_init(&empty,0,MAX);
